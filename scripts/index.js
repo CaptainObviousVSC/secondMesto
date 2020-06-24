974b6ce825d5a947debfb6ccf560398b6b3ed3dd
EASTATE <azifazi1594@gmail.com>
2020-06-24 14:24:10 +0300

    внесены правки

--- scripts/index.js
+++ scripts/index.js
@@ -5,14 +5,16 @@ const popupSaveButton = popup.querySelector('.popup__save');
 const name = document.querySelector('.profile__name');
 const about = document.querySelector('.profile__description');
 const form = document.querySelector('.popup__form');
-const nameInput = form.querySelector('.popup__name');
-const aboutInput = form.querySelector('.popup__about');
+const nameInput = form.querySelector('.popup__input');
+const aboutInput = form.querySelector('.popup__input_about');
 
 
 function popupToggle () {
-    popup.classList.toggle('popup_opened')
-    nameInput.value = name.textContent;
-    aboutInput.value = about.textContent;
+    if (!popup.classList.contains('popup_opened')) {    
+        nameInput.value = name.textContent;
+        aboutInput.value = about.textContent;
+    };
+    popup.classList.toggle('popup_opened');
 };
  popupOpenButton.addEventListener('click', popupToggle);
  popupCloseButton.addEventListener('click', popupToggle);
@@ -23,7 +25,7 @@ function formSubmitHandler (evt) {
 	evt.preventDefault(); 
     name.textContent =  nameInput.value;
  about.textContent = aboutInput.value;
- popupToggle()
+ popupToggle();
 };
 form.addEventListener('submit', formSubmitHandler);
 
